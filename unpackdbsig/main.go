1e9d0ad359a5ca2550e9bffa93e9c5bef900b1bc
Steven Masley <steven@factom.com>
2017-07-03 16:13:49 -0500

    Unpack

---
+++ unpackdbsig/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/FactomProject/factomd/common/messages"
+)
+
+func main() {
+	a := new(messages.DirectoryBlockSignature)
+	v, err := hex.DecodeString(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	nd, err := a.UnmarshalBinaryData(v)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(nd) > 0 {
+		fmt.Printf("Length of bytes remaining is %d", len(nd))
+	}
+
+	fmt.Println(a.String())
+	str, err := json.Marshal(a)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(str))
+}
