6856d1d6a5e0092aecd02dddc6d9560688691348
Emyrk <stevenmasley@gmail.com>
2019-06-28 18:14:50 -0500

    Updated a regex test

--- benchmarks/regex_test.go
+++ benchmarks/regex_test.go
@@ -1,33 +1,50 @@
 package benchmarks_test
 
 import (
-	"testing"
+	"fmt"
 	"regexp"
 	"strings"
+	"testing"
+
+	"github.com/pegnet/pegnet/support"
 )
 
+var haystack string
+var haystackBytes []byte
+var needle string
+
+var realisticHaystackString = "2049799 14:10:03.160    8195-:-0 FollowerExecute                        M-be9992|R-c349a8|H-be9992|0xc022b38780                        EOM[ 0]:   EOM-  DBh/VMh/h 8195/2/-- minute 0 FF  0 --Leader[455b7b] hash[be9992] local"
+var realisticHaystackBytes = []byte(realisticHaystackString)
+var realisticNeedle = "455b7b"
+
+func init() {
+	haystackBytes = support.RandomByteSliceOfLen(50)
+	haystack = fmt.Sprintf("%x", haystackBytes)
+
+	needle = haystack[80:86]
+}
+
 func BenchmarkRegex(b *testing.B) {
-	r, _ := regexp.Compile(".*test.*")
-	s := []byte("BLAHtest")
+	r, _ := regexp.Compile("[a-f0-9]6")
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		r.Find(s)
+		r.Find(realisticHaystackBytes)
 	}
 }
 
 func BenchmarkContains(b *testing.B) {
-	s := "BLAHtest"
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		strings.Contains(s, "test")
+		strings.Contains(realisticNeedle, realisticHaystackString)
 	}
 }
 
-func TestRegex(t *testing.T) {
-	r, _ := regexp.Compile(".*test.*")
-	s := []byte("BLAHtest")
-	var _, _ = s, r
-}
+//
+//func TestRegex(t *testing.T) {
+//	r, _ := regexp.Compile("test")
+//	s := []byte("BLAHtest")
+//	var _, _ = s, r
+//}
 
 // func BenchmarkFoo(b *testing.B) {
 // 	for i := 0; i < b.N; i++ {
@@ -35,3 +52,7 @@ func TestRegex(t *testing.T) {
 // 		// perform the operation we're analyzing
 // 	}
 // }
+
+// 100 char
+// last half
+// 6 char hex
